02db0c7c72d45a81439199a7b2599f8b3ea50425
AbbasQamar408 <qamar.abbas@siliconplex.com>
2026-01-31 11:41:51 +0500

    redux persistor added

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { store, persistor } from './store'
 import App from './App.tsx'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,60 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { api } from './services/api';
 import authReducer from './slices/authSlice';
 import logger from 'redux-logger';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+
+// 1. Combine all your reducers
+const rootReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
+  auth: authReducer,
+})
+
+
+// 2. Configure persistence
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['auth'], // Only persist the auth slice
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+
+// export const store = configureStore({
+//   reducer: {
+//     // Standard slices
+//     auth: authReducer,
+//     // RTK Query api reducer
+//     [api.reducerPath]: api.reducer,
+//   },
+//   // Middleware is required for RTK Query to handle caching, invalidation, and polling
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware(({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     })).concat(api.middleware,logger),
+// });
+
 export const store = configureStore({
-  reducer: {
-    // Standard slices
-    auth: authReducer,
-    // RTK Query api reducer
-    [api.reducerPath]: api.reducer,
-  },
-  // Middleware is required for RTK Query to handle caching, invalidation, and polling
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware,logger),
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore redux-persist actions (they use non-serializable values)
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(api.middleware,logger),
 });
 
-setupListeners(store.dispatch);
+// setupListeners(store.dispatch);
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
